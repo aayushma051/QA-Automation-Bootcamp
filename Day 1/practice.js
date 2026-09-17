e3f92419531c5ca7fcc28b5e2db7647db063e317
aayushma051 <aayushma051@gmail.com>
2026-09-17 21:03:39 +0545

    Add files via upload

---
+++ Day 1/practice.js
@@ -0,0 +1,13 @@
+console.log("Aayushma Khimbaja") 
+console.log("QA automation");
+
+
+let firstName ="Aayushma";
+firstName = "Aayu";
+let lastName ="Khimbaja";
+
+
+
+var number= 12;
+console
+number = 1;
