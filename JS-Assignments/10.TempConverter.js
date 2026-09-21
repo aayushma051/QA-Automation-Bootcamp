ef0f2c13156d34ec628a59e4ef0c40c4f04dc97b
Aayushma Khimbaja <aayushma051@gmail.com>
2026-09-21 16:53:18 +0545

    Delete JS-Assignments directory
    delete message

--- JS-Assignments/10.TempConverter.js
+++
@@ -1,8 +0,0 @@
-// Q10. Temperature Converter (arrow function)
-// Task: Write an arrow function cToF that converts Celsius to Fahrenheit using the formula (C × 9/5) + 32. Return the result.
-
-
-const cToF = (c) => (c * 9/5) + 32;
-
-console.log(cToF(0));
-console.log(cToF(100));
